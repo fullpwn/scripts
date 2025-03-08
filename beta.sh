e4e7ecaa4b89985d35a650289ed794df29b73326
miss australiana <git@missaustraliana.net>
2025-03-08 17:52:59 +1100

    chile

--- beta.sh
+++ beta.sh
@@ -38,10 +38,10 @@ for (( i=0; i<$1; ++i)); do
 	fi
 	#echo $prt
 #    sudo docker run --env DOWNLOADER=$USERNAME --env HTTP_USERNAME= --env HTTP_PASSWORD= --env SELECTED_PROJECT=$PROJECT --env SHARED_RSYNC_THREADS=6 --detach --name s$i --label=.com.centurylinklabs.watchtower.enable=true --log-driver json-file --log-opt max-size=50m --publish 80$prt:8001 --restart=on-failure atdr.meo.ws/archiveteam/warrior-dockerfile
-    sudo docker run -d --name s$i --label=com.centurylinklabs.watchtower.enable=true --restart=unless-stopped atdr.meo.ws/archiveteam/urls-grab --concurrent 17 fullpwnmedia
+    sudo docker run -d --name s$i --label=com.centurylinklabs.watchtower.enable=true --restart=unless-stopped atdr.meo.ws/archiveteam/urls-grab --concurrent 16 fullpwnmedia
 #echo "session$i $USERNAME $PROJECT"
 done
 
 
-sudo docker container stats
+#sudo docker container stats
 #curl https://missaustraliana.net/warrior/netdocker.sh | sh
