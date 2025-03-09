9f6049e1de252a8eb722fff00010bf2bc3177f63
miss australiana <git@missaustraliana.net>
2025-03-09 17:24:38 +1100

    hmm

--- netdocker.sh
+++ netdocker.sh
@@ -10,7 +10,7 @@ echo "--------------------"
     PRRXRD=$(head /sys/class/net/docker0/statistics/rx_bytes)
 while true
 do 
-    
+    DISK=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
     TX=$(head /sys/class/net/docker0/statistics/tx_bytes)
     ptx="↓ $(numfmt --to iec --format "%8.4f" $TX)"
     RX=$(head /sys/class/net/docker0/statistics/rx_bytes)
@@ -27,6 +27,11 @@ do
     tput rc
     printf "$ptx $prx\033[0K\r\n"
     printf "↓ $TXFRM/s ↑ $RXFRM/s\033[0K\r\n"
+    
+    if [ $DISK -gt 20 ]
+    then
+        printf "Disk usage: $DISK%$ETEOL\033[0K\r\n"
+    fi
     if [ $CONTUPD -eq $CONTCNT ]
     then
         printf "All containers healthy.$ETEOL\033[0K\r\n"
